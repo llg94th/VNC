eb51549432a153c9599409780e4da101d56916a0
llg94th <llg94th@gmail.com>
2016-08-03 14:03:24 +0700

    VNCnoti

---
+++ app/src/main/java/com/example/ngoc/vncgiaohngpro/VNC.java
@@ -0,0 +1,9 @@
+package com.example.ngoc.vncgiaohngpro;
+
+/**
+ * Created by HUNGNTPH04073@FPT.EDU.VN on 8/3/2016.
+ */
+public class VNC {
+    public final static String API_GET_LIST_NOTI = "http://localhost/VNCCapital/notification/get-list.php";
+    public final static String API_GET_NOTI_BY_ID = "http://localhost/VNCCapital/notification/template.php?noti_id=";
+}
---
+++ app/src/main/java/com/example/ngoc/vncgiaohngpro/objects/VNCNotification.java
@@ -0,0 +1,62 @@
+package com.example.ngoc.vncgiaohngpro.objects;
+
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by HUNGNTPH04073@FPT.EDU.VN on 8/3/2016.
+ */
+public class VNCNotification {
+    private int id;
+    private String name;
+    private String content;
+    private long time;
+
+    public VNCNotification() {
+    }
+
+    public VNCNotification(int id, String name, long time) {
+        this.id = id;
+        this.name = name;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        byte[] data = Base64.decode(name,Base64.DEFAULT);
+        try {
+            return new String(data, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return name;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
